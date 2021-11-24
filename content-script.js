50f9c4b7d37b5b2bda424b940ba0865358cd2254
Saad Altabari <saadtabari@gmail.com>
2021-11-23 21:03:39 -0800

    update js event to only prevent submitting negative language when clicking enter

--- content-script.js
+++ content-script.js
@@ -1,23 +1,21 @@
 function validateText(text) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://127.0.0.1:5000/validate');
+    xhr.open("POST", 'http://127.0.0.1:5000/validate', false);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
-    xhr.onreadystatechange = function () {
-       if (xhr.readyState === 4 && xhr.status == 200) {
-          response_json = JSON.parse(xhr.responseText);
-          if (response_json.is_toxic){
-            alert("Hi There! This does not sound very nice! Perhaps there is a better way to say it? Can you reword and rephrase it?")
-          }
-       }
-    };
     xhr.send("text="+text);
+   if (xhr.status === 200) {
+      response_json = JSON.parse(xhr.responseText);
+      return response_json.is_toxic
+   }
 }
 
 function writingHandler(e) {
     if (e.keyCode === 13){
-        validateText(e.target.innerText);
-        e.preventDefault();
+        if (validateText(e.target.innerText)) {
+            e.preventDefault();
+            alert("Hi There! This does not sound very nice! Perhaps there is a better way to say it? Can you reword and rephrase it?")
+        }
     }
 }
 
